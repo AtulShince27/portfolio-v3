9d67f8a21c8a2ee2701f4f57de10c36b02745ed0
AtulShince27 <atulk.shince@gmail.com>
2024-11-25 17:13:35 +0530

    Changed Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://AtulShince27.github.io',
+  site: 'https://atulshince27.github.io',
   base: '/portfolio-v3/',
   integrations: [tailwind()],
   build: {
