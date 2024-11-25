737254fb7857ceed9837e298c9eb5e5b4d805db4
AtulShince27 <atulk.shince@gmail.com>
2024-11-25 17:10:08 +0530

    Added Github Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,10 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://AtulShince27.github.io',
+  base: '/portfolio-v3/',
   integrations: [tailwind()],
+  build: {
+    assets: 'assets'
+  },
 });
