e9abecb28dbae12b4ef6e9036dd40f8f13a569e3
AtulShince27 <atulk.shince@gmail.com>
2025-03-10 21:31:04 +0530

    Package Updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://AtulShince27.github.io/portfolio-v3/',
   integrations: [tailwind()],
   build: {
     assets: 'assets'
   },
+  adapter: vercel(),
 });
